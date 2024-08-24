1eae29f7d88c6f4f6a3f165e80ac52abc02e11da
Gabriel de Melo Tizziani <126525209+gabrieltizziani@users.noreply.github.com>
2024-08-23 21:01:17 -0300

    Finalizando

--- config/database.js
+++ config/database.js
@@ -1,11 +1,35 @@
 module.exports = {
+  development: {
     dialect: 'postgres',
     host: 'localhost',
-    username: 'usuario',
-    password: 'senha',
-    database: 'apinode',
+    username: 'postgres',
+    password: '280305',
+    database: 'api-node',
     define: {
-        timestamps: true,
-        underscored: true
+      timestamps: true,
+      underscored: true
     }
-}
+  },
+  test: {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '280305',
+    database: 'api-node_test',
+    define: {
+      timestamps: true,
+      underscored: true
+    }
+  },
+  production: {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '280305',
+    database: 'api-node_production',
+    define: {
+      timestamps: true,
+      underscored: true
+    }
+  }
+};
--- server.js
+++ server.js
@@ -1,35 +1,12 @@
 const express = require("express");
-const axios = require("axios");
+const routes = require("./routes"); // Verifique se o caminho está correto
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+app.use(express.json());
+app.use('/api', routes); // Corrigido de '/api,' para '/api'
 
-app.get("/novarota", (req, res) => {
-  res.send("Nova rota criada.");
-});
-
-app.get("/consulta-cep/:cep", async (req, res) => {
-    const cep = req.params.cep;
-    const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
-  
-    try {
-      if (!cepRegex.test(cep)) {
-        return res.status(400).send('CEP inválido. Formato: XXXXX-XXX');
-      }else{
-        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-        res.json(response.data);
-      }
-    } catch (error) {
-      console.error("Erro ao fazer requisição", error);
-      res.status(500).send("Erro ao consultar o CEP");
-    }
-  });
-  
-
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
